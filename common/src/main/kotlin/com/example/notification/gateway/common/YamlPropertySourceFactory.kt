a771d306896b23063899e6ea5da3f384baef783d
Armen Nazaretyan <nazaretyan.armen@gmail.com>
2023-12-05 16:50:12 +0400

    Added api-application and api-rest modules

---
+++ common/src/main/kotlin/com/example/notification/gateway/common/YamlPropertySourceFactory.kt
@@ -0,0 +1,24 @@
+package com.example.notification.gateway.common
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
+import org.springframework.core.env.PropertiesPropertySource
+import org.springframework.core.env.PropertySource
+import org.springframework.core.io.support.EncodedResource
+import org.springframework.core.io.support.PropertySourceFactory
+
+
+class YamlPropertySourceFactory : PropertySourceFactory {
+
+    override fun createPropertySource(name: String?, encodedResource: EncodedResource): PropertySource<*> {
+        val factory = YamlPropertiesFactoryBean().apply {
+            setResources(encodedResource.resource)
+        }
+
+        val properties = factory.getObject()
+
+        return PropertiesPropertySource(
+            encodedResource.resource.filename!!,
+            properties!!
+        )
+    }
+}
