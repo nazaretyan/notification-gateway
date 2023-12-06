30b0af380ff843b11e0ea77c74665df7264c77f6
Armen Nazaretyan <nazaretyan.armen@gmail.com>
2023-12-06 18:25:27 +0400

    Move sources to other module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,45 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
-	id("org.springframework.boot") version "3.2.0"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") apply false
+	id("io.spring.dependency-management")
 
-	kotlin("jvm") version "1.9.20"
-	kotlin("plugin.spring") version "1.9.20"
-	kotlin("plugin.jpa") version "1.9.20"
+	kotlin("jvm") apply false
+	kotlin("plugin.spring") apply false
 }
 
-group = "com.example.notification"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+	group = "com.example.notification"
+	version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_21
-}
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
+	repositories {
+		mavenCentral()
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+subprojects {
+	apply {
+		plugin("io.spring.dependency-management")
+
+		plugin("org.jetbrains.kotlin.jvm")
+		plugin("org.jetbrains.kotlin.plugin.spring")
+	}
+
+	dependencyManagement {
+		imports {
+			mavenBom(SpringBootPlugin.BOM_COORDINATES)
+		}
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs += "-Xjsr305=strict"
+			jvmTarget = "21"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
+pluginManagement {
+    plugins {
+        id("org.springframework.boot") version "3.2.0"
+        id("io.spring.dependency-management") version "1.1.4"
+
+        kotlin("jvm") version "1.9.20"
+        kotlin("plugin.spring") version "1.9.20"
+        kotlin("plugin.jpa") version "1.9.20"
+    }
+}
+
 rootProject.name = "notification-gateway"
+
+include("api:api-application")
